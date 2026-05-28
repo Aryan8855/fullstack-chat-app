2023c196c8e10c53824ccc0f184450013b6f804b
Aryan8855 <aryangiri8855@gmail.com>
2026-05-28 17:39:44 +0530

    updated frontend backend ecs deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "http://13.204.222.4:5000/api",
+
   withCredentials: true,
 });
